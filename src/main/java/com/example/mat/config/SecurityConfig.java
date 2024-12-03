48f187268b8dcda61081f453eb0f13db80f56a66
신건호 <shingh1283@gmail.com>
2024-12-03 18:03:49 +0900

    241203 shin

---
+++ src/main/java/com/example/mat/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.example.mat.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableMethodSecurity
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/", "/assets/**", "/css/**", "/js/**", "/upload/**").permitAll()
+                .anyRequest().permitAll());
+        // http.formLogin(login ->
+        // login.loginPage("/member/login").permitAll().defaultSuccessUrl("/mat/list"));
+
+        // http.sessionManagement(session ->
+        // session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS));
+        // http.logout(logout -> logout.logoutRequestMatcher(new
+        // AntPathRequestMatcher("/member/logout"))
+        // .logoutSuccessUrl("/"));
+        // http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+}
