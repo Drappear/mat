fc7700486039b1af05bc42bd1c34f9bf6356d752
hajin kim <khj020416@gmail.com>
2024-12-30 15:49:18 +0900

    Recipe: 남은 것 [list 페이지] : category btn 클릭 시, 해당 카테고리값을 가진 card 만 화면에 뿌리기, pagination / [create] 페이지: RecipeImage, RecipeStep 이미지 업로드 안됨 / [read 페이지]: 로드 문제, viewCount 중복 막기

--- src/test/java/com/example/mat/repository/RecipeRepositoryTest.java
+++ src/test/java/com/example/mat/repository/RecipeRepositoryTest.java
@@ -1,8 +1,13 @@
 package com.example.mat.repository;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Commit;
 
 import com.example.mat.entity.recipe.Recipe;
@@ -28,42 +33,53 @@ public class RecipeRepositoryTest {
     @Autowired
     private RecipeStepRepository recipeStepRepository;
 
-
-    //TODO: 레시피 추가
+    // TODO: 레시피 추가
     @Commit
     @Transactional
     @Test
-    public void testRecipeAdd(){
+    public void testRecipeAdd() {
         Recipe recipe = Recipe.builder()
-        .content("")
-        .serving("")
-        .time("")
-        .difficulty("")
-        .build();
+                .content("")
+                .serving("")
+                .time("")
+                .difficulty("")
+                .build();
         recipeRepository.save(recipe);
     }
 
-    //TODO: 레시피 카테고리 추가
+    // TODO: 레시피 카테고리 추가
     @Commit
     @Transactional
     @Test
-    public void testCategoryAdd(){
+    public void testCategoryAdd() {
         RecipeCategory recipeCategory = RecipeCategory.builder()
-        // .name("반찬")
-        // .name("국/탕/찌개")
-        // .name("디저트")
-        // .name("면/만두")
-        // .name("밥/죽/떡")
-        .name("퓨전")
-        // .name("김치/젓갈/장류")
-        // .name("양념/소스/잼")
-        // .name("양식")
-        // .name("차/음료/술")
-        .build();
+                // .name("반찬")
+                // .name("국/탕/찌개")
+                // .name("디저트")
+                // .name("면/만두")
+                // .name("밥/죽/떡")
+                .name("퓨전")
+                // .name("김치/젓갈/장류")
+                // .name("양념/소스/잼")
+                // .name("양식")
+                // .name("차/음료/술")
+                .build();
         recipeCategoryRepository.save(recipeCategory);
     }
 
     // TODO: 레시피 재료 추가
 
+    // 레시피 전체 리스트 가져오기
+    @Test
+    public void testListPage() {
+
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("rno").descending());
+
+        Page<Object[]> result = recipeRepository.getListPage(pageRequest);
+
+        for (Object[] objects : result) {
+            System.out.println(Arrays.toString(objects));
+        }
+    }
 
 }
