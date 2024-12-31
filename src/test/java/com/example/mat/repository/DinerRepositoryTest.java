6cbf584f3b3008f1556098dd44762f7af94012ee
Drappear <drappear@gmail.com>
2024-12-31 11:22:06 +0900

    테스트용 더미데이터

--- src/test/java/com/example/mat/repository/DinerRepositoryTest.java
+++ src/test/java/com/example/mat/repository/DinerRepositoryTest.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.annotation.Commit;
 
+import com.example.mat.entity.Image;
 import com.example.mat.entity.constant.MemberRole;
 import com.example.mat.entity.diner.Diner;
 import com.example.mat.entity.diner.DinerCategory;
@@ -25,6 +26,8 @@ import jakarta.transaction.Transactional;
 public class DinerRepositoryTest {
   @Autowired
   private DinerRepository dinerRepository;
+  @Autowired
+  private ImageRepository imageRepository;
 
   @Autowired
   private DinerCategoryRepository dinerCategoryRepository;
@@ -80,7 +83,59 @@ public class DinerRepositoryTest {
     dinerCategoryRepository.save(DinerCategory.builder().name("퓨전&기타").build());
   }
 
-  // 
+  // 식당 정보 등록
+  @Test
+  public void dinerInsertTest() {
+    IntStream.rangeClosed(1, 10).forEach(i -> {
+      int num = (int) (Math.random() * 6) + 1;
+      Diner diner = Diner.builder()
+          .address("서울 종로구")
+          .addressDetail(111 * i + "" + 111 * i)
+          .content("안녕하세요 " + i + "번 식당 소개글입니다.")
+          .menu("메뉴1 : 000000, 메뉴2 : 000000, 메뉴3 : 000000")
+          .name(i + "번 식당")
+          .phone(111 * i + "-" + 1111 * i + "-" + 1111 * i)
+          .regNum(111 * i + "-" + 11 * i + "-" + 11111 * i)
+          .workTime("오전 00:00 ~ 00:00, 점심시간 00:00 ~ 00:00, 오후 00:00 ~ 00:00")
+          .viewCount(0L)
+          .dinerCategory(DinerCategory.builder().dcid(Long.valueOf(num)).build())
+          .build();
 
+      dinerRepository.save(diner);
+    });
+  }
+
+  // 식당 사진
+  @Test
+  public void insertDinerImageTest() {
+    IntStream.rangeClosed(1, 10).forEach(i -> {
+
+      Image image1 = Image.builder()
+          .imgCate(1)
+          .imgName("diner" + i + ".jpg")
+          .path("2024/12/31")
+          .uuid("d" + i)
+          .diner(Diner.builder().did(Long.valueOf(i)).build())
+          .build();
+      Image image2 = Image.builder()
+          .imgCate(1)
+          .imgName("food" + i + ".jpg")
+          .path("2024/12/31")
+          .uuid("f" + i)
+          .diner(Diner.builder().did(Long.valueOf(i)).build())
+          .build();
+      Image image3 = Image.builder()
+          .imgCate(1)
+          .imgName("food" + 11 * i + ".jpg")
+          .path("2024/12/31")
+          .uuid("f" + 11 * i)
+          .diner(Diner.builder().did(Long.valueOf(i)).build())
+          .build();
+
+      imageRepository.save(image3);
+      imageRepository.save(image2);
+      imageRepository.save(image1);
+    });
+  }
 
 }
