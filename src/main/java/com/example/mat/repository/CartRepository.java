94120ad191ff133f7eea96624d2c35717e6e2321
신건호 <shingh1283@gmail.com>
2025-02-13 17:45:36 +0900

    회원탈퇴 때문에 여러분의 레파지토리 수정 좀 했습니다

--- src/main/java/com/example/mat/repository/CartRepository.java
+++ src/main/java/com/example/mat/repository/CartRepository.java
@@ -1,16 +1,18 @@
 package com.example.mat.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.mat.entity.market.Cart;
 import java.util.List;
 import com.example.mat.entity.shin.Member;
 
-
-
-public interface CartRepository extends JpaRepository<Cart, Long>{
+public interface CartRepository extends JpaRepository<Cart, Long> {
     // 현재 로그인한 멤버의 카트 찾기
     Cart findByMember(Member member);
-    
+
+    @Modifying
+    @Query("DELETE FROM Cart c WHERE c.member =:member")
+    void deleteByMember(Member member);
 }
