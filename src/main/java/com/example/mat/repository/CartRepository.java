e1b81a0e4f9207a7a17d5ba2d0b41967cb13e5e1
신건호 <shingh1283@gmail.com>
2025-02-14 16:22:25 +0900

    250214

--- src/main/java/com/example/mat/repository/CartRepository.java
+++ src/main/java/com/example/mat/repository/CartRepository.java
@@ -1,24 +1,16 @@
 package com.example.mat.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-=======
 import org.springframework.stereotype.Repository;
->>>>>>> 5483c4b598ad365e84282cfc2861d229dc5ddbb5
 
 import com.example.mat.entity.market.Cart;
 import java.util.List;
 import com.example.mat.entity.shin.Member;
 
-<<<<<<< HEAD
-public interface CartRepository extends JpaRepository<Cart, Long> {
-=======
-
 @Repository
-public interface CartRepository extends JpaRepository<Cart, Long>{
->>>>>>> 5483c4b598ad365e84282cfc2861d229dc5ddbb5
+public interface CartRepository extends JpaRepository<Cart, Long> {
     // 현재 로그인한 멤버의 카트 찾기
     Cart findByMember(Member member);
 
