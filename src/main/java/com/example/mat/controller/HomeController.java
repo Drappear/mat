a7df96e922fcf3cda4e1a4eba09e8653fbe7cb3a
Drappear <drappear@gmail.com>
2025-02-19 14:39:31 +0900

    메인 페이지 작업 중

--- src/main/java/com/example/mat/controller/HomeController.java
+++ src/main/java/com/example/mat/controller/HomeController.java
@@ -1,16 +1,30 @@
 package com.example.mat.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.example.mat.dto.PageRequestDto;
+import com.example.mat.dto.PageResultDto;
+import com.example.mat.dto.diner.DinerDto;
+import com.example.mat.service.DinerService;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
+@RequiredArgsConstructor
 @Controller
 public class HomeController {
 
+  private final DinerService dinerService;
+
   @GetMapping("/main")
-  public void getMain() {
+  public void getMain(@ModelAttribute("requestDto") PageRequestDto pageRequestDto, Model model) {
     log.info("main 요청");
+    pageRequestDto.setSize(8);
+    PageResultDto<DinerDto, Object[]> dinerResult = dinerService.getDinerList(pageRequestDto);
+    model.addAttribute("dinerResult", dinerResult);
   }
 }
